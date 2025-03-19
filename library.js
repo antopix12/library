e953abc9143577b76a26d12f173fe758f14fb2bc
antopix <antopix33@gmail.com>
2025-03-19 11:01:19 -0700

    constructor and addBookToLibrary complete, type checking added

--- library.js
+++ library.js
@@ -1,9 +1,38 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+
+  if (typeof title !== "string") {
+    throw new TypeError("title must be a string.");
+  }
+  if (typeof author !== "string") {
+      throw new TypeError("author must be a string.");
+  }
+  if (typeof pages !== "number" || !Number.isInteger(pages) || pages < 1) {
+      throw new TypeError("pages must be a positive integer.");
+  }
+  if (typeof read !== "boolean") {
+      throw new TypeError("read must be a boolean.");
+  }
+
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
   // take params, create a book then store it in the array
+
+  if (!(book instanceof Book)) {
+    throw new TypeError("Attempted to add an invalid book to the library.");
+  }
+  myLibrary.push(book);
 }
+
+let dune = new Book("Dune", "Frank Herbert", 896, true);
+addBookToLibrary(dune);
+
+let dune2 = new Book("Dune Messiah", "Frank Herbet", 288, true);
+addBookToLibrary(dune2);
